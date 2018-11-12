59d0fea6d9ce56580b1985b34eda151aaebd8d7e
sfeek <shane.feek@gmail.com>
2018-11-12 16:18:51 -0500

    Added null hypothesis check

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CSVtoP
 Perform Statistical t-Test with p-Values from two CSV files.
 
-Usage: csvtop <fileA.csv> <fileB.csv>
+Usage: csvtop <confidence_level> <fileA.csv> <fileB.csv>
 
 To compile: make
--- src/csvtop.c
+++ src/csvtop.c
@@ -304,7 +304,7 @@ int main (int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf ("\n\n Usage: csvtop <confidence level> <fname A.csv> <fname B>.csv\n");
+		printf ("\n\n Usage: csvtop <confidence_level> <fname A.csv> <fname B>.csv\n");
 		exit (EXIT_FAILURE);
 	}
 
